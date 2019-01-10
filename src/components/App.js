d882e1822bcedf41ab46f5ade494c776372166e1
musab.akram <musab.akram@numumail.com>
2019-01-10 11:44:19 +0500

    streams routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamShow from "./streams/StreamShow";
 import StreamList from "./streams/StreamList";
 import StreamDelete from "./streams/StreamDelete";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
+
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
